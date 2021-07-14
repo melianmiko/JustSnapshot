eb822d9c87272f9ea79d15da7329a006f3648d93
MelianMiko <melianmiko@gmail.com>
2021-07-14 10:45:29 +0700

    CLI script splitted

--- setup.py
+++ setup.py
@@ -3,8 +3,14 @@ from setuptools import setup
 setup(
     name='JustSnapshot',
     version='0.1',
-    packages=['jsnapshot_cli', 'jsnapshot_core'],
-    scripts=["bin/jsnapshot"],
+    packages=['jsnapshot_core'],
+    scripts=[
+        "scripts/jsnapshot-create",
+        "scripts/jsnapshot-delete",
+        "scripts/jsnapshot-list",
+        "scripts/jsnapshot-setup",
+        "scripts/jsnapshot-show"
+    ],
     url='https://melianmiko.ru/',
     license='BSD',
     author='MelianMiko',
